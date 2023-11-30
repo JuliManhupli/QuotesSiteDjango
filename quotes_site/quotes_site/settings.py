aa0c4018be8778634563f593373c2c6da4a23910
JuliManhupli <juliamangup@gmail.com>
2023-11-30 09:55:09 +0200

    Fixed bugs

--- quotes_site/quotes_site/settings.py
+++ quotes_site/quotes_site/settings.py
@@ -15,13 +15,13 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()  # Завантажує змінні середовища з файлу .env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=)&9ienz6^gni30(ogfxy1h1ddfiywra3p4x$n-lb*ds*f)l&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'quotes_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-load_dotenv()  # Завантажує змінні середовища з файлу .env
+
 
 DATABASES = {
     'default': {
